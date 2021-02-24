e70ccc5cb0bffa2ca7ed2d161d5da0e324739d61
Hardik <hardik.routray@rutgers.edu>
2021-02-23 21:00:58 -0500

    update bias

--- Bias.py
+++ Bias.py
@@ -49,7 +49,7 @@ for j in range(len(ctaus)):
         print "Looking at ctau----------",ctaus[j], "----------------"
 
         # os.chdir("./mass_{}".format(mass))
-        for file in glob.glob("datacard*allbins.txt"):
+        for file in glob.glob("datacard*allbinsbias.txt"):
                 if "ctau{}_".format(ctaus[j]) in file:
                         print(file)
                         cardname = file
@@ -72,8 +72,8 @@ for j in range(len(ctaus)):
         bias2err.append(ctaus[j])
 
         # Signal Injection
-        ntoys = 500
-        name = "analysis"
+        ntoys = 50
+        # name = "analysis"
         INJ = [0.,2.]
 
 
@@ -88,23 +88,45 @@ for j in range(len(ctaus)):
 
             # os.system("combine %s -M GenerateOnly -t %d -m %f --saveToys --toysFrequentist --expectSignal %f -n %s%f --bypassFrequentistFit" %(cardname, ntoys, mass, i, name, i))
 
-            os.system("combine %s -M GenerateOnly -t %d -m %f --saveToys --toysFrequentist --expectSignal %f -n %s%f --bypassFrequentistFit --setParameters   pdf_index223=0,pdf_index312=0,pdf_index111=0,pdf_index112=0,pdf_index113=0,pdf_index121=0,pdf_index122=0,pdf_index123=0,pdf_index211=0,pdf_index212=0,pdf_index213=0,pdf_index221=0,pdf_index222=0,pdf_index311=0,pdf_index313=0,pdf_index321=0,pdf_index322=0,pdf_index323=0,pdf_index411=0,pdf_index412=0,pdf_index413=0,pdf_index421=0,pdf_index422=0,pdf_index423=0,pdf_index511=0,pdf_index512=0,pdf_index513=0,pdf_index521=0,pdf_index522=0,pdf_index523=0,pdf_index611=0,pdf_index612=0,pdf_index613=0,pdf_index621=0,pdf_index622=0,pdf_index623=0   --freezeParameters   pdf_index223,pdf_index312,pdf_index111,pdf_index112,pdf_index113,pdf_index121,pdf_index122,pdf_index123,pdf_index211,pdf_index212,pdf_index213,pdf_index221,pdf_index222,pdf_index311,pdf_index313,pdf_index321,pdf_index322,pdf_index323,pdf_index411,pdf_index412,pdf_index413,pdf_index421,pdf_index422,pdf_index423,pdf_index511,pdf_index512,pdf_index513,pdf_index521,pdf_index522,pdf_index523,pdf_index611,pdf_index612,pdf_index613,pdf_index621,pdf_index622,pdf_index623" %(cardname, ntoys, mass, i, name, i))
+            nbatches = 5
+            for batch in range(1,nbatches+1):
 
-            if num_after_point(mass) == 0:
-                    os.system("combine -M FitDiagnostics -d %s -m %f -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%i.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy  --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i))
+                print "running on batch", batch
 
-            elif num_after_point(mass) == 1:
-                    os.system("combine -M FitDiagnostics -d %s -m %f -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%.1f.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy  --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i))
+                name = "0{}analysis".format(batch)
 
-            elif num_after_point(mass) == 2:
-                    os.system("combine -M FitDiagnostics -d %s -m %f  -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%.2f.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy  --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i))
+                toycmd = "combine %s -M GenerateOnly -t %d -m %f --saveToys --toysFrequentist --expectSignal %f -n %s%f --bypassFrequentistFit --setParameters   pdf_index223=0,pdf_index312=0,pdf_index111=0,pdf_index112=0,pdf_index113=0,pdf_index121=0,pdf_index122=0,pdf_index123=0,pdf_index211=0,pdf_index212=0,pdf_index213=0,pdf_index221=0,pdf_index222=0,pdf_index311=0,pdf_index313=0,pdf_index321=0,pdf_index322=0,pdf_index323=0,pdf_index411=0,pdf_index412=0,pdf_index413=0,pdf_index421=0,pdf_index422=0,pdf_index423=0,pdf_index511=0,pdf_index512=0,pdf_index513=0,pdf_index521=0,pdf_index522=0,pdf_index523=0,pdf_index611=0,pdf_index612=0,pdf_index613=0,pdf_index621=0,pdf_index622=0,pdf_index623=0   --freezeParameters   pdf_index223,pdf_index312,pdf_index111,pdf_index112,pdf_index113,pdf_index121,pdf_index122,pdf_index123,pdf_index211,pdf_index212,pdf_index213,pdf_index221,pdf_index222,pdf_index311,pdf_index313,pdf_index321,pdf_index322,pdf_index323,pdf_index411,pdf_index412,pdf_index413,pdf_index421,pdf_index422,pdf_index423,pdf_index511,pdf_index512,pdf_index513,pdf_index521,pdf_index522,pdf_index523,pdf_index611,pdf_index612,pdf_index613,pdf_index621,pdf_index622,pdf_index623" %(cardname, ntoys, mass, i, name, i)
 
-            elif num_after_point(mass) == 3:
-                    os.system("combine -M FitDiagnostics -d %s -m %f -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%.3f.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i))
+                os.system(toycmd)
+
+                fitcmd0 = "combine -M FitDiagnostics -d %s -m %f -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%i.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy  --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i)
+
+                fitcmd1 = "combine -M FitDiagnostics -d %s -m %f -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%.1f.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy  --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i)
+
+                fitcmd2 = "combine -M FitDiagnostics -d %s -m %f -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%.2f.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy  --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i)
+
+                fitcmd3 = "combine -M FitDiagnostics -d %s -m %f -t %d --toysFile higgsCombine%s%f.GenerateOnly.mH%.3f.123456.root --rMin %f --rMax %f --saveWorkspace -n %s%f --forceRecreateNLL --keepFailures --saveNormalizations   --savePredictionsPerToy  --saveWithUncertainties --cminDefaultMinimizerStrategy=0" %(cardname, mass, ntoys, name, i, mass, rmin, rmax, name, i)
 
 
+                if num_after_point(mass) == 0:
+                    os.system(fitcmd0)
+
+                elif num_after_point(mass) == 1:
+                    os.system(fitcmd1)
+
+                elif num_after_point(mass) == 2:
+                    os.system(fitcmd2)
+
+                elif num_after_point(mass) == 3:
+                    os.system(fitcmd3)
+
+
+            os.system("hadd fitDiagnosticscombined_r%i.root fitDiagnostics0*analysis%f.root" %(i,i))    
+            
             # Plot
-            F = ROOT.TFile("fitDiagnostics%s%f.root" % (name, i))
+            # F = ROOT.TFile("fitDiagnostics%s%f.root" % (name, i))
+            F = ROOT.TFile("fitDiagnosticscombined_r%i.root" % (i)) 
+
             T = F.Get("tree_fit_sb")
 
             H = ROOT.TH1F("Bias Test, injected r="+str(int(i)),
@@ -151,14 +173,14 @@ for j in range(len(ctaus)):
                 bias2err.append(H.GetStdDev())
 
 
-            os.system('rm higgsCombineanalysis2.000000.FitDiagnostics.mH{}.123456.root'.format(mass))
-            os.system('rm higgsCombineanalysis2.000000.GenerateOnly.mH{}.123456.root'.format(mass))
-            os.system('rm fitDiagnosticsanalysis2.000000.root')
+            # os.system('rm higgsCombineanalysis2.000000.FitDiagnostics.mH{}.123456.root'.format(mass))
+            # os.system('rm higgsCombineanalysis2.000000.GenerateOnly.mH{}.123456.root'.format(mass))
+            # os.system('rm fitDiagnosticsanalysis2.000000.root')
 
 
-            os.system('rm higgsCombineanalysis0.000000.FitDiagnostics.mH{}.123456.root'.format(mass))
-            os.system('rm higgsCombineanalysis0.000000.GenerateOnly.mH{}.123456.root'.format(mass))
-            os.system('rm fitDiagnosticsanalysis0.000000.root')
+            # os.system('rm higgsCombineanalysis0.000000.FitDiagnostics.mH{}.123456.root'.format(mass))
+            # os.system('rm higgsCombineanalysis0.000000.GenerateOnly.mH{}.123456.root'.format(mass))
+            # os.system('rm fitDiagnosticsanalysis0.000000.root')
 
 
         df_bias0 = pd.DataFrame([bias0],columns=['mass', 'ctau', 'bern'])
