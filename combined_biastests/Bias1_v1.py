962e3f5e3b945ef0111f77f010df907567881339
Hardik <hardik.routray@rutgers.edu>
2021-03-18 21:11:09 -0400

    update scripts

--- combined_biastests/Bias1_v1.py
+++ combined_biastests/Bias1_v1.py
@@ -20,45 +20,8 @@ def num_after_point(x):
         return 0
     return len(s) - s.index('.') - 1
 
-def getpdfindex(toselect="backgroundexpo"):
-    fnames = glob.glob("simple-shapes-*.root")
-    params = []
-    for fname in fnames:
-        f = ROOT.TFile(fname)
-        mybkgs = f.Get("mybkgs")
-        mybkgs.cd()
-
-        indexname = None
-        for key in ROOT.gDirectory.GetList():
-            name = key.GetName()
-            if name.startswith("pdf_index"):
-                indexname = name
-                binnum = name.split("index")[1]
-                break
-
-        multipdf = ROOT.gDirectory.Get("roomultipdf_{}".format(binnum))
-        n = multipdf.getNumPdfs()
-        bestindex = 0
-        for i in range(n):
-            name = str(multipdf.getPdf(i).getPlotLabel())
-            if name == toselect + "_" + binnum:
-                bestindex = i
-                break
-
-        params.append((indexname, bestindex))
-
-    
-    string1 = ",".join(["{}={}".format(*p) for p in params])
-    string2 = ",".join([p[0] for p in params])
-    
-    string = " --setParameters " + string1 + " --freezeParameters " + string2
-
-    return string
-
 
 masseslist = [0.5,0.525,0.55,0.575,0.6,0.625,0.65,0.675,0.7,0.725,0.75,0.775,0.8,0.825,0.85,0.875,0.9,0.925,0.95,1.25,1.5,1.75,2,2.24,2.4,2.5,2.6,2.76,3,3.25,3.5,3.75,4,4.24,4.52,4.76,5,5.5,6,6.6,7,7.49,8,8.4,9,9.5,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-
-masseslist = [4.24,4.52,4.76,5,5.5,6,6.6,7,7.49,8,8.4,9,9.5,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
 # masseslist = [2]
 
@@ -81,16 +44,12 @@ print "Running on mass", mass
 
 os.chdir("mass_{}".format(mass))
 
-pdfindexexpo = getpdfindex("backgroundexpo")
-print pdfindexexpo
-# exit()
-
 for j in range(len(ctaus)):
 
         print "Looking at ctau----------",ctaus[j], "----------------"
 
         # os.chdir("./mass_{}".format(mass))
-        for file in glob.glob("datacard*allbinsbias.txt"):
+        for file in glob.glob("datacard*allbinsbias_v1.txt"):
                 if "ctau{}_".format(ctaus[j]) in file:
                         print(file)
                         cardname = file
@@ -122,7 +81,8 @@ for j in range(len(ctaus)):
         # Signal Injection
         ntoys = 50
         # name = "analysis"
-        INJ = [0.]
+        INJ = [2.,5.]
+
 
         for i in INJ:
 
@@ -145,9 +105,7 @@ for j in range(len(ctaus)):
 
                 name = "0{}_{}_analysis".format(batch,ctaus[j])
                 # '''
-                # toycmd = "combine %s -M GenerateOnly -t %d -m %f --saveToys --toysFrequentist --expectSignal %f -n %s%f --bypassFrequentistFit --setParameters   pdf_index223=0,pdf_index312=0,pdf_index111=0,pdf_index112=0,pdf_index113=0,pdf_index121=0,pdf_index122=0,pdf_index123=0,pdf_index211=0,pdf_index212=0,pdf_index213=0,pdf_index221=0,pdf_index222=0,pdf_index311=0,pdf_index313=0,pdf_index321=0,pdf_index322=0,pdf_index323=0,pdf_index411=0,pdf_index412=0,pdf_index413=0,pdf_index421=0,pdf_index422=0,pdf_index423=0,pdf_index511=0,pdf_index512=0,pdf_index513=0,pdf_index521=0,pdf_index522=0,pdf_index523=0,pdf_index611=0,pdf_index612=0,pdf_index613=0,pdf_index621=0,pdf_index622=0,pdf_index623=0   --freezeParameters   pdf_index223,pdf_index312,pdf_index111,pdf_index112,pdf_index113,pdf_index121,pdf_index122,pdf_index123,pdf_index211,pdf_index212,pdf_index213,pdf_index221,pdf_index222,pdf_index311,pdf_index313,pdf_index321,pdf_index322,pdf_index323,pdf_index411,pdf_index412,pdf_index413,pdf_index421,pdf_index422,pdf_index423,pdf_index511,pdf_index512,pdf_index513,pdf_index521,pdf_index522,pdf_index523,pdf_index611,pdf_index612,pdf_index613,pdf_index621,pdf_index622,pdf_index623 -s %i" %(cardname, ntoys, mass, i, name, i, batch)
-
-                toycmd = "combine %s -M GenerateOnly -t %d -m %f --saveToys --toysFrequentist --expectSignal %f -n %s%f --bypassFrequentistFit -s %i" %(cardname, ntoys, mass, i, name, i, batch) + pdfindexexpo
+                toycmd = "combine %s -M GenerateOnly -t %d -m %f --saveToys --toysNoSystematics --expectSignal %f -n %s%f --bypassFrequentistFit --setParameters   pdf_index223=0,pdf_index312=0,pdf_index111=0,pdf_index112=0,pdf_index113=0,pdf_index121=0,pdf_index122=0,pdf_index123=0,pdf_index211=0,pdf_index212=0,pdf_index213=0,pdf_index221=0,pdf_index222=0,pdf_index311=0,pdf_index313=0,pdf_index321=0,pdf_index322=0,pdf_index323=0,pdf_index411=0,pdf_index412=0,pdf_index413=0,pdf_index421=0,pdf_index422=0,pdf_index423=0,pdf_index511=0,pdf_index512=0,pdf_index513=0,pdf_index521=0,pdf_index522=0,pdf_index523=0,pdf_index611=0,pdf_index612=0,pdf_index613=0,pdf_index621=0,pdf_index622=0,pdf_index623=0   --freezeParameters   pdf_index223,pdf_index312,pdf_index111,pdf_index112,pdf_index113,pdf_index121,pdf_index122,pdf_index123,pdf_index211,pdf_index212,pdf_index213,pdf_index221,pdf_index222,pdf_index311,pdf_index313,pdf_index321,pdf_index322,pdf_index323,pdf_index411,pdf_index412,pdf_index413,pdf_index421,pdf_index422,pdf_index423,pdf_index511,pdf_index512,pdf_index513,pdf_index521,pdf_index522,pdf_index523,pdf_index611,pdf_index612,pdf_index613,pdf_index621,pdf_index622,pdf_index623 -s %i" %(cardname, ntoys, mass, i, name, i, batch)
 
                 os.system(toycmd)
 
@@ -184,15 +142,15 @@ for j in range(len(ctaus)):
                           "Bias Test;(r_{measured} - r_{injected})/#sigma_{r};toys", 100, -5., 5.)
             # T.Draw("(r-%f)/rErr>>Bias Test, injected r=%d" %(i, int(i)))
 
-            T.Draw("(r-%f)/((r<%f)*rHiErr + (r>%f)*rLoErr)>>Bias Test, injected r=%d" %(i,i,i,int(i)))
+            # T.Draw("(r-%f)/((r<%f)*rHiErr + (r>%f)*rLoErr)>>Bias Test, injected r=%d" %(i,i,i,int(i)))
 
-            # if mass < 3:
+            if mass < 3:
 
-            #     T.Draw("(r-%f)/((r<%f)*rHiErr + (r>%f)*rLoErr)>>Bias Test, injected r=%d" %(i,i,i,int(i)))
+                T.Draw("(r-%f)/((r<%f)*rHiErr + (r>%f)*rLoErr)>>Bias Test, injected r=%d" %(i,i,i,int(i)))
 
-            # else:
+            else:
 
-            #     T.Draw("(r-%f)/(rHiErr)>>Bias Test, injected r=%d" %(i,int(i)),"abs(rLoErr)/r > 0.01")
+                T.Draw("(r-%f)/(rHiErr)>>Bias Test, injected r=%d" %(i,int(i)),"abs(rLoErr)/r > 0.01")
 
             # H = ROOT.TH1F("Bias Test, injected r="+str(int(i)),
             #               "Signal Injection Test;r_{measured};toys", 100, -50., 50.)
@@ -249,12 +207,12 @@ for j in range(len(ctaus)):
             # os.system('rm fitDiagnosticsanalysis0.000000.root')
 
 
-        df_bias0 = pd.DataFrame([bias0],columns=['mass', 'ctau', 'bern'])
-        df_bias0err = pd.DataFrame([bias0err],columns=['mass', 'ctau', 'bern'])
-        # df_bias2 = pd.DataFrame([bias2],columns=['mass', 'ctau', 'bern'])
-        # df_bias2err = pd.DataFrame([bias2err],columns=['mass', 'ctau', 'bern'])
-        # df_bias5 = pd.DataFrame([bias5],columns=['mass', 'ctau', 'bern'])
-        # df_bias5err = pd.DataFrame([bias5err],columns=['mass', 'ctau', 'bern'])
+        # df_bias0 = pd.DataFrame([bias0],columns=['mass', 'ctau', 'bern'])
+        # df_bias0err = pd.DataFrame([bias0err],columns=['mass', 'ctau', 'bern'])
+        df_bias2 = pd.DataFrame([bias2],columns=['mass', 'ctau', 'bern'])
+        df_bias2err = pd.DataFrame([bias2err],columns=['mass', 'ctau', 'bern'])
+        df_bias5 = pd.DataFrame([bias5],columns=['mass', 'ctau', 'bern'])
+        df_bias5err = pd.DataFrame([bias5err],columns=['mass', 'ctau', 'bern'])
 
         if not os.path.exists("biascsvs"):
             os.makedirs("biascsvs")
@@ -262,12 +220,12 @@ for j in range(len(ctaus)):
         # if not os.path.exists("rootfiles"):
         #     os.makedirs("rootfiles")
 
-        df_bias0.to_csv('biascsvs/bias0_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
-        df_bias0err.to_csv('biascsvs/bias0err_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
-        # df_bias2.to_csv('biascsvs/bias2_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
-        # df_bias2err.to_csv('biascsvs/bias2err_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
-        # df_bias5.to_csv('biascsvs/bias5_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
-        # df_bias5err.to_csv('biascsvs/bias5err_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
+        # df_bias0.to_csv('biascsvs/bias0_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
+        # df_bias0err.to_csv('biascsvs/bias0err_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
+        df_bias2.to_csv('biascsvs/bias2_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
+        df_bias2err.to_csv('biascsvs/bias2err_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
+        df_bias5.to_csv('biascsvs/bias5_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
+        df_bias5err.to_csv('biascsvs/bias5err_mass{}_ctau{}_v0.csv'.format(mass,ctaus[j]),index=False)
 
 
 os.chdir("./..")
